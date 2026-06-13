25e7943c10769f82d0eb4fff86ecadd486cee5e1
Sikandarr56 <f2024408043@umt.edu.pk>
2026-06-13 20:03:45 +0500

    Initial project structure

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("CyberShield Toolkit")
+root.geometry("800x600")
+
+label = tk.Label(root, text="CyberShield Toolkit")
+label.pack(pady=20)
+
+root.mainloop()
